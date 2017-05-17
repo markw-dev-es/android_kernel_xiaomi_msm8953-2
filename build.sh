8d1ac142dc3a7191b955fac2a831efdaca4b2d1d
Felipe de Jesús <felipealcocer30@gmail.com>
2017-05-16 21:19:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOCAL_DIR=pwd
-export PATH="$PATH:/home/felipe/android/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.3.1/bin/"
+export PATH="$PATH:/home/felipe/android/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.4.1/bin/"
 export CROSS_COMPILE="aarch64-linux-gnu-"
 mkdir -p out
 make ARCH=arm64 O=out lineageos_markw_defconfig
