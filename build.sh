929bcf86aae638f57ccf553c445a607b6c8de88f
GuardianX <felipealcocer30@gmail.com>
2017-05-30 22:51:22 -0500

    Eliminados ajustes opcionales, agregado adreno idler y agregada compatibilidad con formatos EXFAT y NTFS

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LOCAL_DIR=pwd
-export PATH="$PATH:/home/felipe/LineageOS13/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-4.9.4/bin/"
+export PATH="$PATH:/home/felipe/LineageOS13/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.4.1/bin/"
 export CROSS_COMPILE="aarch64-linux-gnu-"
 mkdir -p out
-make ARCH=arm64 O=out lineageos_markw_defconfig
+make ARCH=arm64 O=out markw_defconfig
 make ARCH=arm64 O=out -j2
 
