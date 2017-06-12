043efece291265e4b5a97896c0fb24f867a92bec
paloda <paloda09@gmail.com>
2017-06-12 22:42:50 +0200

    update linaro

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,12 +9,12 @@ BUILDING_DIR=$OUT_DIR/kernel_obj
 JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 DATE=`date +%m-%d-%H:%M`
 
-CROSS_COMPILER=/home/msdx321/workspace/android/toolchains/linaro-4.9.4/bin/aarch64-linux-gnu-
+CROSS_COMPILER=/home/paloda/android/toolchains/aarch64-linux-gnu-5.4.1/bin/aarch64-linux-gnu-
 
 ANYKERNEL_DIR=$ROOT_DIR/misc/anykernel2
 TEMP_DIR=$OUT_DIR/temp
 
-DEFCONFIG=$1
+DEFCONFIG=markw_defconfig
 
 FUNC_PRINT()
 {
