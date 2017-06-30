49076ce90d5368c7dd7e25ef591b72207a54fcf2
Felipe de Jesús <felipealcocer30@gmail.com>
2017-06-30 09:58:50 -0500

    Actualizado kernel a versión 3.18.59 (EOL)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LOCAL_DIR=pwd
-export PATH="$PATH:/home/felipe/LineageOS13/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.4.1/bin/"
-export CROSS_COMPILE="aarch64-linux-gnu-"
+export PATH="$PATH:/home/felipe/LineageOS14/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-5.4.1/bin/"
+export CROSS_COMPILE="aarch64-linux-android-"
 mkdir -p out
-make ARCH=arm64 O=out markw_defconfig
+make ARCH=arm64 O=out miui_markw_defconfig
 make ARCH=arm64 O=out -j2
 
