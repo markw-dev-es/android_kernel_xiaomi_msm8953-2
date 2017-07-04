2cafde7beabc080225c8790def8c18870880e4e9
Felipe de Jesús <felipealcocer30@gmail.com>
2017-07-03 22:45:35 -0500

    Actualización de drivers de forma general

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#export JACK_SERVER_VM_ARGUMENTS="Dfile.encoding=UTF-8 -XX: +TieredCompilation -Xmx3072m"
+#./prebuilts/sdk/tools/jack-admin kill-server
+#./prebuilts/sdk/tools/jack-admin start-server
 
 LOCAL_DIR=pwd
 export PATH="$PATH:/home/guardianx/LineageOS14/system/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-5.4.1/bin/"
