28c83c86f54ad3faf193772e465f7448e8003dd7
paloda <paloda09@gmail.com>
2017-09-13 11:11:17 +0200

    building

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ BUILDING_DIR=$OUT_DIR/kernel_obj
 JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 DATE=`date +%m-%d-%H:%M`
 
-CROSS_COMPILER=//home/paloda/android/cocina/markw_kernel/aarch64-linux-android-5.3-uber/bin/aarch64-linux-android-
+CROSS_COMPILER=/home/paloda/cocina/linaro-4.9/bin/aarch64-linux-android-
 
 ANYKERNEL_DIR=$ROOT_DIR/misc/anykernel2
 TEMP_DIR=$OUT_DIR/temp
@@ -48,9 +48,12 @@ FUNC_PACK()
 		FUNC_PRINT "Start Packing"
 		cp -r $ANYKERNEL_DIR/* $TEMP_DIR
 		cp $BUILDING_DIR/arch/arm64/boot/Image.gz-dtb $TEMP_DIR/zImage-dtb
+        mkdir $TEMP_DIR/modules
+        find . -type f -name "wlan.ko" | xargs cp -t $TEMP_DIR/modules
+        find $TEMP_DIR -iname "wlan.ko" -exec /home/paloda/cocina/linaro-4.9/bin/aarch64-linux-android-strip --strip-debug {} \;
 		cd $TEMP_DIR
-		zip -r9 mKernel.zip ./*
-		mv mKernel.zip $OUT_DIR/mKernel-$DATE.zip
+		zip -r9 palodakernel.zip ./*
+		mv palodakernel.zip $OUT_DIR/palodakernel-$DATE.zip
 		cd $ROOT_DIR
 		FUNC_PRINT "Finish Packing"
 }
